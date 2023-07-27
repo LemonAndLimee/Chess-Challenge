034b22fcdbf503c9323894127782c2553407d856
LemonAndLimee <brockweir75@gmail.com>
2023-07-27 15:46:28 +0100

    Evaluation function changed
    - Rewritten evaluation function to consider positions instead of moves
    - The transposition table now stores positions

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,150 +1,101 @@
 ﻿using ChessChallenge.API;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 public class MyBot : IChessBot
 {
 
-    //Issue: doesn't seem to recognise dangerous moves, ignores pieces in danger (due to lack of assessment of position it leaves it on)
     //Endgames and openings need work as they lead to repetition
 
-    //had one case of illegal move when trying to ladder checkmate black with two queens vs 1 king
-    //currently does worse with a depth of 4 than a depth of 2
+    //issue found with transposition table: move which is previously safe and now dangerous is considered safe by computer
+    //thus allowing opponent to walk up and checkmate with no objections
+    //only new moves are actually calculated
+    //need a check on pieces newly in danger: bot will move a 0 score move even if a different piece is in danger
+    //can be solved by storing positions not moves?
 
-    PieceType[] types = new PieceType[] { PieceType.None, PieceType.Pawn, PieceType.Knight, PieceType.Bishop, PieceType.Rook, PieceType.Queen, PieceType.King };
-    short[] values = new short[] { 0, 10, 30, 30, 50, 90, 100 };
+    //endgame: incentivise moving opp. king to the edge plus moving our king closer to opp king
 
-    Dictionary<Move, int> transpositionTable = new Dictionary<Move, int>();
+
+    PieceType[] types = new PieceType[] { PieceType.Pawn, PieceType.Knight, PieceType.Bishop, PieceType.Rook, PieceType.Queen, PieceType.King };
+    short[] values = new short[] { 10, 30, 30, 50, 90, 1000 };
+
+    Dictionary<ulong, int> transpositionTable = new Dictionary<ulong, int>();
+    Move currentBestMove;
+    int calculations = 0;
 
     public Move Think(Board board, Timer timer)
     {
-        return CalculateMove(board, 4);
+        calculations = 0;
+        Search(board, 4, -10000, 10000);
+        Console.WriteLine("dictionary = " + transpositionTable.Count);
+        Console.WriteLine("calc = " + calculations);
+        return currentBestMove;
     }
-
-    public Move CalculateMove(Board b, int depth)
+    public int Search(Board b, int depth, int best, int upperBound) //lower bound is current best move, upper bound is best move of opponent
     {
-        int d = depth - 1;
-        Move[] moves = b.GetLegalMoves();
-
-        Move bestMove = new Move();
-        int highestScore = -1000;
-
-        foreach (Move move in moves)
+        if (transpositionTable.ContainsKey(b.ZobristKey))
         {
-            int worstCase = 1000;
-            if (transpositionTable.ContainsKey(move))
-            {
-                //use score from table
-                worstCase = transpositionTable[move];
-            }
-            else
-            {
-                //work out, then add to table
-                b.MakeMove(move);
-                if (b.IsInCheckmate())
-                {
-                    b.UndoMove(move);
-                    return move;
-                }
-                int score1 = GetScore(move);
-                foreach (Move opponentMove in b.GetLegalMoves())
-                {
-                    b.MakeMove(opponentMove);
-                    if (b.IsInCheckmate())
-                    {
-                        worstCase = -10000;
-                    }
-                    else
-                    {
-                        int score2 = score1 - GetScore(opponentMove);
-                        if (d > 0)
-                        {
-                            score2 += GetScore(CalculateMove(b, d));
-                        }
+            return transpositionTable[b.ZobristKey];
+        }
+        if (b.IsInCheckmate())
+        {
+            return -1000;
+        }
 
-                        if (score2 < worstCase)
-                        {
-                            worstCase = score2;
-                        }
-                        if (score2 < highestScore)
-                        {
-                            b.UndoMove(opponentMove);
-                            break;
-                        }
-                    }
-                    b.UndoMove(opponentMove);
+        if (depth == 0)
+        {
+            return EvaluateScore(b);
+        }
+        else
+        {
+            int bestScore = best;
+            Move[] moves = b.GetLegalMoves();
+            
+            if (moves.Length == 0)
+            {
+                return 0;
+            }
+            Move bestMove = moves[0];
+
+            foreach (Move move in moves)
+            {
+                calculations++;
+                b.MakeMove(move);
+                int score = -Search(b, depth - 1, -upperBound, -bestScore);
+                if (score >= upperBound)
+                {
+                    bestScore = upperBound;
+                    b.UndoMove(move);
+                    break;
                 }
-                //what about old moves that now mean something different? they will be in the table but contain wrong values
-                //unsure if move data type contains the piece info or not
-                transpositionTable[move] = worstCase;
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = move;
+                }
+                if (depth == 4)
+                {
+                    transpositionTable[b.ZobristKey] = bestScore;
+                }
                 b.UndoMove(move);
             }
-            
-
-            if (worstCase >= highestScore)
-            {
-                highestScore = worstCase;
-                bestMove = move;
-            }
+            currentBestMove = bestMove;
+            return bestScore;
         }
 
-        Console.WriteLine("dict length = " + transpositionTable.Count);
-
-        return bestMove;
     }
-    public int GetScore(Move m)
+    public int EvaluateScore(Board board)
     {
         int score = 0;
-        if (m.IsCapture)
+
+        for (int i = 0; i < types.Length; i++)
         {
-            score += values[Array.IndexOf(types, m.CapturePieceType)];
-        }
-        if (m.IsPromotion)
-        {
-            score += values[Array.IndexOf(types, m.PromotionPieceType)];
+            //will be called from POV of person to play
+            score += values[i] * board.GetPieceList(types[i], board.IsWhiteToMove).Count;
+            score -= values[i] * board.GetPieceList(types[i], !board.IsWhiteToMove).Count;
         }
         return score;
     }
-
-    /**accounted for piece captures, piece promotions, checkmates
-     * not yet accounted for checks, might be needed?
-     * also no code specifically for endgames
-     * 
-     * 
-     * function f(b, depth): depth = 1 means check my moves and opps responses
-     *  depth--
-     *  get legal moves
-     *  
-     *  highest score;
-     *  hs move;
-     *  foreach legal move:
-     *      make move
-     *      if board is in checkmate then undo move, return move
-     *      s1 = score(move)
-     *      worst case; 
-     *      foreach now legal move (opp turn)
-     *          make opp move
-     *          if board in checkmate then worst case = -10000
-     *          else then
-     *              s2 = s1 - score(oppmove)  //s2 is new current score from our pov
-     *              if depth > 0 then
-     *                  s2 += score(f(b, depth))
-     *              if s2 <= worst case then worst case = s2
-     *          undo opp move
-     *      
-     *      if worst case > highest score then highest score = worst case, hs move = move
-     *      undo move
-     *  
-     *  return hs move */
-
-    /** scoring function(b, m):
-     *  score = 0
-     *  if capture then score += capture piece worth
-     *  if promotion then score += promotion piece worth
-     *  
-     *  return score
-     */
-
-
 
 }
